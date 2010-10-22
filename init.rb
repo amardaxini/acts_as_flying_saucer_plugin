79db5ac1bb87a6191d66e5d3b8c9ad72ac1d97e2
amardaxini <amardaxini@gmail.com>
2010-10-22 12:05:51 +0530

    Removed required daemons iine

--- init.rb
+++ init.rb
@@ -3,5 +3,4 @@ require 'config'
 require 'xhtml2pdf'
 require 'acts_as_flying_saucer_controller'
 require 'acts_as_flying_saucer_view'
-require 'daemons'
 ActionController::Base.send(:include, ActsAsFlyingSaucer::Controller)
